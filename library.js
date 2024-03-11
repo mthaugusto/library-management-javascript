25e4635ae6caaeddd85d94c9b05d58500990192b
mthaugusto <mthaugusto94@icloud.com>
2024-03-10 22:01:38 -0300

    Pequenas alterações JQUERY

--- library.js
+++ library.js
@@ -1,20 +1,22 @@
+// USANDO JQUERY E NÃO JAVASCRIPT DOM //
+
 let books = [];
 
 function addBook(book) {
   let table = $("#bookTable tbody");
   table.append(`
-            <tr id="${book.id}">
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.genre}</td>
-                <td>${book.year}</td>
-                <td>${book.quantity}</td>
-                <td>
-                    <button class="btn btn-sm btn-warning editBtn" data-id="${book.id}">Edit</button>
-                    <button class="btn btn-sm btn-danger deleteBtn" data-id="${book.id}">Delete</button>
-                </td>
-            </tr>
-        `);
+        <tr id="${book.id}">
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.genre}</td>
+            <td>${book.year}</td>
+            <td>${book.quantity}</td>
+            <td>
+                <button class="btn btn-sm btn-warning editBtn" data-id="${book.id}">Edit</button>
+                <button class="btn btn-sm btn-danger deleteBtn" data-id="${book.id}">Delete</button>
+            </td>
+        </tr>
+    `);
 }
 
 function clearForm() {
